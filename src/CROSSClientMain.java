dfb0a66286f5ba65f5efc5489f8f81f2a0262705
Bolla <dado.delgaudio@gmail.com>
2024-12-23 17:30:30 +0100

    Initial commit

---
+++ src/CROSSClientMain.java
@@ -0,0 +1,12 @@
+public class CROSSClientMain
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Ciao sono il client :D");
+
+		while(true)
+		{
+
+		}
+	}
+}
---
+++ src/CROSSServerMain.java
@@ -0,0 +1,12 @@
+public class CROSSServerMain
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Ciao sono il server :)");
+
+		while(true)
+		{
+
+		}
+	}
+}
